5f259c98ef96d08e59b5ae1d68ab88e014c8622c
sanzianamaieczki <petronela-sinziana.maieczki@student.tuiasi.ro>
2024-05-17 18:38:14 +0300

    update interfete

---
+++ proiect-2024/AdaugareRezervare.Designer.cs
@@ -0,0 +1,281 @@
+﻿namespace proiect_2024
+{
+    partial class AdaugareRezervare
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonLogOut = new System.Windows.Forms.Button();
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.menuPrincipal = new System.Windows.Forms.MenuStrip();
+            this.aboutToolStripMenu = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenu = new System.Windows.Forms.ToolStripMenuItem();
+            this.buttonAdaugaRezervare = new System.Windows.Forms.Button();
+            this.comboBoxTipBalcon = new System.Windows.Forms.ComboBox();
+            this.comboBoxCapacitate = new System.Windows.Forms.ComboBox();
+            this.comboBoxTipulCamerei = new System.Windows.Forms.ComboBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.textBoxPretNoapte = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.checkIn = new System.Windows.Forms.MonthCalendar();
+            this.checkOut = new System.Windows.Forms.MonthCalendar();
+            this.buttonRezervari = new System.Windows.Forms.Button();
+            this.menuPrincipal.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // buttonLogOut
+            // 
+            this.buttonLogOut.Location = new System.Drawing.Point(1079, 28);
+            this.buttonLogOut.Name = "buttonLogOut";
+            this.buttonLogOut.Size = new System.Drawing.Size(103, 38);
+            this.buttonLogOut.TabIndex = 64;
+            this.buttonLogOut.Text = "Log Out";
+            this.buttonLogOut.UseVisualStyleBackColor = true;
+            // 
+            // toolStrip1
+            // 
+            this.toolStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.toolStrip1.Location = new System.Drawing.Point(0, 28);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.Size = new System.Drawing.Size(1182, 25);
+            this.toolStrip1.TabIndex = 63;
+            this.toolStrip1.Text = "toolStrip1";
+            // 
+            // menuPrincipal
+            // 
+            this.menuPrincipal.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuPrincipal.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.aboutToolStripMenu,
+            this.exitToolStripMenu});
+            this.menuPrincipal.Location = new System.Drawing.Point(0, 0);
+            this.menuPrincipal.Name = "menuPrincipal";
+            this.menuPrincipal.Size = new System.Drawing.Size(1182, 28);
+            this.menuPrincipal.TabIndex = 62;
+            this.menuPrincipal.Text = "menuPrincipal";
+            // 
+            // aboutToolStripMenu
+            // 
+            this.aboutToolStripMenu.Name = "aboutToolStripMenu";
+            this.aboutToolStripMenu.Size = new System.Drawing.Size(64, 24);
+            this.aboutToolStripMenu.Text = "About";
+            // 
+            // exitToolStripMenu
+            // 
+            this.exitToolStripMenu.Name = "exitToolStripMenu";
+            this.exitToolStripMenu.Size = new System.Drawing.Size(47, 24);
+            this.exitToolStripMenu.Text = "Exit";
+            // 
+            // buttonAdaugaRezervare
+            // 
+            this.buttonAdaugaRezervare.Location = new System.Drawing.Point(489, 564);
+            this.buttonAdaugaRezervare.Name = "buttonAdaugaRezervare";
+            this.buttonAdaugaRezervare.Size = new System.Drawing.Size(224, 57);
+            this.buttonAdaugaRezervare.TabIndex = 78;
+            this.buttonAdaugaRezervare.Text = "Adauga Rezervare";
+            this.buttonAdaugaRezervare.UseVisualStyleBackColor = true;
+            // 
+            // comboBoxTipBalcon
+            // 
+            this.comboBoxTipBalcon.FormattingEnabled = true;
+            this.comboBoxTipBalcon.Items.AddRange(new object[] {
+            "Fara",
+            "Propriu",
+            "Comun"});
+            this.comboBoxTipBalcon.Location = new System.Drawing.Point(608, 179);
+            this.comboBoxTipBalcon.Name = "comboBoxTipBalcon";
+            this.comboBoxTipBalcon.Size = new System.Drawing.Size(224, 24);
+            this.comboBoxTipBalcon.TabIndex = 77;
+            this.comboBoxTipBalcon.Text = "Fara";
+            // 
+            // comboBoxCapacitate
+            // 
+            this.comboBoxCapacitate.FormattingEnabled = true;
+            this.comboBoxCapacitate.Items.AddRange(new object[] {
+            "1",
+            "2",
+            "3",
+            "4",
+            "5",
+            "6"});
+            this.comboBoxCapacitate.Location = new System.Drawing.Point(608, 125);
+            this.comboBoxCapacitate.Name = "comboBoxCapacitate";
+            this.comboBoxCapacitate.Size = new System.Drawing.Size(224, 24);
+            this.comboBoxCapacitate.TabIndex = 76;
+            this.comboBoxCapacitate.Text = "1";
+            // 
+            // comboBoxTipulCamerei
+            // 
+            this.comboBoxTipulCamerei.FormattingEnabled = true;
+            this.comboBoxTipulCamerei.Items.AddRange(new object[] {
+            "Standard",
+            "Deluxe",
+            "Apartament"});
+            this.comboBoxTipulCamerei.Location = new System.Drawing.Point(608, 69);
+            this.comboBoxTipulCamerei.Name = "comboBoxTipulCamerei";
+            this.comboBoxTipulCamerei.Size = new System.Drawing.Size(224, 24);
+            this.comboBoxTipulCamerei.TabIndex = 75;
+            this.comboBoxTipulCamerei.Text = "Standard";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(350, 187);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(72, 16);
+            this.label7.TabIndex = 74;
+            this.label7.Text = "Tip Balcon";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(349, 133);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(141, 16);
+            this.label4.TabIndex = 73;
+            this.label4.Text = "Capacitate (persoane)";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(349, 77);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(91, 16);
+            this.label2.TabIndex = 72;
+            this.label2.Text = "Tipul Camerei";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(350, 240);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(143, 16);
+            this.label5.TabIndex = 69;
+            this.label5.Text = "Pret pe noapte (numar)";
+            // 
+            // textBoxPretNoapte
+            // 
+            this.textBoxPretNoapte.Enabled = false;
+            this.textBoxPretNoapte.Location = new System.Drawing.Point(608, 240);
+            this.textBoxPretNoapte.Name = "textBoxPretNoapte";
+            this.textBoxPretNoapte.Size = new System.Drawing.Size(224, 22);
+            this.textBoxPretNoapte.TabIndex = 67;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(274, 295);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(58, 16);
+            this.label1.TabIndex = 79;
+            this.label1.Text = "Check In";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(661, 295);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(68, 16);
+            this.label3.TabIndex = 80;
+            this.label3.Text = "Check Out";
+            // 
+            // checkIn
+            // 
+            this.checkIn.Location = new System.Drawing.Point(277, 316);
+            this.checkIn.Name = "checkIn";
+            this.checkIn.TabIndex = 81;
+            // 
+            // checkOut
+            // 
+            this.checkOut.Location = new System.Drawing.Point(664, 316);
+            this.checkOut.Name = "checkOut";
+            this.checkOut.TabIndex = 82;
+            // 
+            // buttonRezervari
+            // 
+            this.buttonRezervari.Location = new System.Drawing.Point(0, 28);
+            this.buttonRezervari.Name = "buttonRezervari";
+            this.buttonRezervari.Size = new System.Drawing.Size(103, 38);
+            this.buttonRezervari.TabIndex = 83;
+            this.buttonRezervari.Text = "Rezervari";
+            this.buttonRezervari.UseVisualStyleBackColor = true;
+            // 
+            // AdaugareRezervare
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1182, 653);
+            this.Controls.Add(this.buttonRezervari);
+            this.Controls.Add(this.checkOut);
+            this.Controls.Add(this.checkIn);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.buttonAdaugaRezervare);
+            this.Controls.Add(this.comboBoxTipBalcon);
+            this.Controls.Add(this.comboBoxCapacitate);
+            this.Controls.Add(this.comboBoxTipulCamerei);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.textBoxPretNoapte);
+            this.Controls.Add(this.buttonLogOut);
+            this.Controls.Add(this.toolStrip1);
+            this.Controls.Add(this.menuPrincipal);
+            this.Name = "AdaugareRezervare";
+            this.Text = "Adaugare Rezervare";
+            this.menuPrincipal.ResumeLayout(false);
+            this.menuPrincipal.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button buttonLogOut;
+        private System.Windows.Forms.ToolStrip toolStrip1;
+        private System.Windows.Forms.MenuStrip menuPrincipal;
+        private System.Windows.Forms.ToolStripMenuItem aboutToolStripMenu;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenu;
+        private System.Windows.Forms.Button buttonAdaugaRezervare;
+        private System.Windows.Forms.ComboBox comboBoxTipBalcon;
+        private System.Windows.Forms.ComboBox comboBoxCapacitate;
+        private System.Windows.Forms.ComboBox comboBoxTipulCamerei;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox textBoxPretNoapte;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.MonthCalendar checkIn;
+        private System.Windows.Forms.MonthCalendar checkOut;
+        private System.Windows.Forms.Button buttonRezervari;
+    }
+}
