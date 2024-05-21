21cae4b9b0fc0302612d3e78ca46d45a8620d092
Butnaru Raimond-Eduard <raimond-eduard.butnaru@student.tuiasi.ro>
2024-05-21 09:22:04 +0300

    Am corectat baza de date si acum am adaugat si o inregistrate pe partea de rezervari

--- proiect-2024/script_populare_initiala.sql
+++ proiect-2024/script_populare_initiala.sql
@@ -6,6 +6,7 @@ INSERT INTO Utilizatori VALUES(1, 'Puscasu', 'Marin', 'mp123', 42, 'user', 'mari
 INSERT INTO Utilizatori (id_utilizator, nume, prenume, username, rol, email, telefon, parola) VALUES(2,'John', 'Doe', 'jd2109', 'Manager', 'john.doe@yahoo.com', '0765432109', 'a80b568a237f50391d2f1f97beaf99564e33d2e1c8a2e5cac21ceda701570312');
 INSERT INTO Utilizatori (id_utilizator, username, rol, parola) VALUES(3,'admin', 'Admin', '8c6976e5b5410415bde908bd4dee15dfb167a9c873fc4bb8a81f6f2ab448a918');
 
+
 --Populare camere
 
 INSERT INTO Camera VALUES(1, 609, 150, 6, 'Deluxe', 'Comun', 4);
@@ -13,4 +14,4 @@ INSERT INTO Camera VALUES(2, 210, 100, 2, 'Standard','Propriu', 1);
 INSERT INTO Camera VALUES(3, 480, 175, 1, 'Apartament','Fara', 2);
 
 
---Populare
+
