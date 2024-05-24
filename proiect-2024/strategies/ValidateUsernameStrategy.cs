4fedb6bc170787bcfb2fbeb6f870b0fb0538c1d4
unknown <vlad-ionut.guriuc@student.tuiasi.ro>
2024-05-24 15:01:12 +0300

    Antete

--- proiect-2024/strategies/ValidateUsernameStrategy.cs
+++ proiect-2024/strategies/ValidateUsernameStrategy.cs
@@ -1,4 +1,27 @@
-﻿using proiect_2024.interfaces;
+﻿/**************************************************************************
+ *                                                                        *
+ *  File:        ValidateUsernameStrategy.cs                              *
+ *  Copyright:   (c) 2024, Butnaru Raimond-Eduard,			              *
+ *			   Maieczki Petronela-Sînziana,			                      *
+ *			   Lupu Andreea-Sabina,				                          *
+ *			   Guriuc Vlad-Ionuț                                          *
+ *  E-mail:                                                               *
+ *       raimond-eduard.butnaru@Student.tuiasi.ro 	                      *
+ *		 petronela-sinziana.maieczki@student.tuiasi.ro		              *
+ *		 andreea-sabina.lupu@student.tuiasi.ro 			                  *
+ *		 vlad-ionut.guriuc@student.tuiasi.ro                              *
+ *  Description:  Clasă folosită pentru validarea numelui de utilizator   *
+ *              astfel încat acesta să nu fie null.                       *
+ *                                                                        *
+ *  This code and information is provided "as is" without warranty of     *
+ *  any kind, either expressed or implied, including but not limited      *
+ *  to the implied warranties of merchantability or fitness for a         *
+ *  particular purpose. You are free to use this source code in your      *
+ *  applications as long as the original copyright notice is included.    *
+ *                                                                        *
+ **************************************************************************/
+
+using proiect_2024.interfaces;
 using System;
 using System.Collections.Generic;
 using System.Linq;
